f9818494a0bb23a21d95e707a7810811da0093b0
IanDLeon <ian.reggie.deleon@gmail.com>
2022-06-07 14:49:33 -0400

    Fully working

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /////////////////////////////////////////////
-////////// Import Our Dependencies //////////
+// Import Our Dependencies
 /////////////////////////////////////////////
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
-const methodOverride = require("method-override")
-const mongoose = require("mongoose") // talk to our database
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 const path = require("path")
 
 
@@ -14,10 +14,9 @@ const path = require("path")
 /////////////////////////////////////////////
 // Setup inputs for our connect function
 const DATABASE_URL = process.env.DATABASE_URL;
-// If you get a warining it will get rid of it. 
 const CONFIG = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 };
 
 // Establish Connection
@@ -25,26 +24,24 @@ mongoose.connect(DATABASE_URL, CONFIG);
 
 // Events for when connection opens/disconnects/errors
 mongoose.connection
-  .on("open", () => console.log("Connected to Mongoose"))
-  .on("close", () => console.log("Disconnected from Mongoose"))
-  .on("error", (error) => console.log(error));
+    .on("open", () => console.log("Connected to Mongoose"))
+    .on("close", () => console.log("Disconnected from Mongoose"))
+    .on("error", (error) => console.log(error));
 
 ////////////////////////////////////////////////
 // Our Models
 ////////////////////////////////////////////////
 // pull schema and model from mongoose
+const { Schema, model } = mongoose;
+// ^^^^same as const Schema = mongoose.Schema and next line with const model = mongoose.model. 
+// ^^^^These two are the same to this one line of code which is called deconstructing
 
-/*const Schema = mongoose.Schema
-const model = mongoose.model*/
 
-// is equal to:
-const { Schema, model } = mongoose
-
-//Make fruits Schema
+// make fruits schema
 const fruitsSchema = new Schema({
-  name: String,
-  color: String,
-  readyToEat: Boolean,
+    name: String,
+    color: String,
+    readyToEat: Boolean,
 });
 
 // make fruit model
@@ -53,9 +50,10 @@ const Fruit = model("Fruit", fruitsSchema);
 /////////////////////////////////////////////////
 // Create our Express Application Object Bind Liquid Templating Engine
 /////////////////////////////////////////////////
+const app = require("liquid-express-views")(express(), { root: [path.resolve(__dirname, 'views/')] })
 
-const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
 
+//### Register our Middleware
 
 
 /////////////////////////////////////////////////////
@@ -66,40 +64,153 @@ app.use(methodOverride("_method")); // override for put and delete requests from
 app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
 app.use(express.static("public")); // serve files from public statically
 
+
 ////////////////////////////////////////////
 // Routes
 ////////////////////////////////////////////
 app.get("/", (req, res) => {
-  res.send("your server is running... better catch it.");
+    res.send("your server is running... better catch it.");
 });
 
 app.get("/fruits/seed", (req, res) => {
-  // array of starter fruits
-  const startFruits = [
-    { name: "Orange", color: "orange", readyToEat: false },
-    { name: "Grape", color: "purple", readyToEat: false },
-    { name: "Banana", color: "orange", readyToEat: false },
-    { name: "Strawberry", color: "red", readyToEat: false },
-    { name: "Coconut", color: "brown", readyToEat: false },
-  ];
+    // array of starter fruits
+    const startFruits = [
+        { name: "Orange", color: "orange", readyToEat: false },
+        { name: "Grape", color: "purple", readyToEat: false },
+        { name: "Banana", color: "orange", readyToEat: false },
+        { name: "Strawberry", color: "red", readyToEat: false },
+        { name: "Coconut", color: "brown", readyToEat: false },
+    ];
 
-  // Delete all fruits
-  Fruit.deleteMany({}).then((data) => {
-    // Seed Starter Fruits
-    Fruit.create(startFruits).then((data) => {
-      // send created fruits as response to confirm creation
-      res.json(data);
+    // Delete all fruits
+    Fruit.deleteMany({}).then((data) => {
+        // Seed Starter Fruits
+        Fruit.create(startFruits).then((data) => {
+            // send created fruits as response to confirm creation
+            res.json(data);
+        });
     });
-  });
 });
+
+// new route
+app.get("/fruits/new", (req, res) => {
+    res.render("fruits/new.liquid");
+});
+
+
 
 // index route
 app.get("/fruits", async (req, res) => {
-  const fruits = await Fruit.find({});
-  res.render("fruits/index.liquid", { fruits });
+    const fruits = await Fruit.find({});
+    res.render("fruits/index.liquid", { fruits });
+});
+
+// create route
+app.post("/fruits", (req, res) => {
+    // check if the readyToEat property should be true or false
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    // create the new fruit
+    Fruit.create(req.body)
+        .then((fruits) => {
+            // redirect user to index page if successfully created item
+            res.redirect("/fruits");
+        })
+        // send error as json
+        .catch((error) => {
+            console.log(error);
+            res.json({ error });
+        });
 });
 
 
+// show route
+app.get("/fruits/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+
+    // find the particular fruit from the database
+    Fruit.findById(id)
+        .then((fruit) => {
+            // render the template with the data from the database
+            res.render("fruits/show.liquid", { fruit });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.json({ error });
+        });
+});
+
+//update route
+app.put("/fruits/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+    // check if the readyToEat property should be true or false
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    // update the fruit
+    Fruit.findByIdAndUpdate(id, req.body, { new: true })
+        .then((fruit) => {
+            // redirect to main page after updating
+            res.redirect("/fruits");
+        })
+        // send error as json
+        .catch((error) => {
+            console.log(error);
+            res.json({ error });
+        });
+});
+
+app.delete("/fruits/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+    // delete the fruit
+    Fruit.findByIdAndRemove(id)
+        .then((fruit) => {
+            // redirect to main page after deleting
+            res.redirect("/fruits");
+        })
+        // send error as json
+        .catch((error) => {
+            console.log(error);
+            res.json({ error });
+        });
+});
+
+
+// edit route
+app.get("/fruits/:id/edit", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+    // get the fruit from the database
+    Fruit.findById(id)
+        .then((fruit) => {
+            // render edit page and send fruit data
+            res.render("fruits/edit.liquid", { fruit });
+        })
+        // send error as json
+        .catch((error) => {
+            console.log(error);
+            res.json({ error });
+        });
+});
+
+//update route
+app.put("/fruits/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+    // check if the readyToEat property should be true or false
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    // update the fruit
+    Fruit.findByIdAndUpdate(id, req.body, { new: true })
+        .then((fruit) => {
+            // redirect to main page after updating
+            res.redirect("/fruits");
+        })
+        // send error as json
+        .catch((error) => {
+            console.log(error);
+            res.json({ error });
+        });
+});
 
 
 //////////////////////////////////////////////
